774dd22d88c0c51bfc8581bd01ecbe350f452008
Bamboooz <bambusixmc@gmail.com>
2023-02-05 23:28:42 +0100

    some refactoring, works on pip installation

---
+++ setup.py
@@ -0,0 +1,46 @@
+import sys
+
+from setuptools import setup
+import os
+
+
+class LibrarySetup:
+    def __init__(self):
+        self.classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Operating System :: Microsoft :: Windows :: Windows 10',
+            'Programming Language :: Python :: 3'
+        ]
+
+        self.required_packages = [
+            'colorama',
+            'psutil'
+        ]
+
+        self.create_dist()
+        self.upload_to_pypi()
+
+    def create_dist(self):
+        setup(
+            name='procspy',
+            version='1.0.0',
+            description='ProcSpy - portable command prompt task manager',
+            long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+            url='https://github.com/Bamboooz/procspy',
+            author='Bamboooz',
+            author_email='bambusixmc@gmail.com',
+            license='MIT',
+            classifiers=self.classifiers,
+            keywords=['python', 'windows', 'linux', 'macos', 'cpu',
+                      'temperature', 'motherboard', 'system-monitor',
+                      'hardware-information', 'network-information'],
+            packages=['os_py', 'scripts'],
+            install_requires=self.required_packages
+        )
+
+    @staticmethod
+    def upload_to_pypi():
+        os.popen(r"twine upload --repository-url https:\\upload.pypi.org\legacy dist\*")
+
+
+LibrarySetup()
