2342ca70375a7d61df82517fb8d601bdf0c98f18
Bamboooz <bambusixmc@gmail.com>
2023-02-09 20:34:51 +0100

    internet and display module

--- prompt/prompt.py
+++ prompt/prompt.py
@@ -1,9 +1,10 @@
-from scripts._common import *
-from system._inet import get_ipv4
+from system._inet import *
 from sensor._cpu import *
 from sensor._mem import *
 from system._sys import *
 from system._time import *
+from system._fs import *
+from sensor._proc import *
 
 PROC_SPY_VER = '1.0.0'
 
@@ -15,10 +16,10 @@ def draw_proc_spy_screen():
     mem_usages = memory_usage()
     swap_usages = swap_usage()
 
-    inline_memory_bar = set_inline_spaces(cpu_models['model_raw'], cpu_usages['usage_raw'],
-                                          mem_usages['usage_raw'], swap_usages['usage_raw'])
-    inline_swap_bar = set_inline_spaces(len(mem_usages['usage']), len(mem_usages['usage_total']),
-                                        len(mem_usages['usage_used']), len(mem_usages['usage_free']))
+    inline_memory_bar = set_inline_spaces(cpu_models['model_raw'], cpu_usages['usage_raw'], mem_usages['usage_raw'], swap_usages['usage_raw'])
+    inline_swap_bar = set_inline_spaces(len(mem_usages['usage']), len(mem_usages['usage_total']), len(mem_usages['usage_used']), len(mem_usages['usage_free']))
+    inline_connection_bar = set_inline_spaces(len(f'Connected:{is_connected()}'), len('NETWORK                         State'))[0]
+    inline_ping_bar = set_inline_spaces(len(f'Ping time:{get_ping_time(True)}'), len('NETWORK                         State'))[0]
 
     proc_spy_screen = f'''(ProcSpy v.{PROC_SPY_VER}) - ({machine_name()}) - ({sys_version()}) - (IP: {get_ipv4()}) - (Uptime: {os_uptime()})
     
@@ -26,7 +27,13 @@ def draw_proc_spy_screen():
     {cpu_usages['usage_bar']}{inline_memory_bar[1]}      total: {mem_usages['usage_total']}{inline_swap_bar[1]}      total: {swap_usages['usage_total']}
     {mem_usages['usage_bar']}{inline_memory_bar[2]}      used:  {mem_usages['usage_used']}{inline_swap_bar[2]}      used:  {swap_usages['usage_used']}
     {swap_usages['usage_bar']}{inline_memory_bar[3]}      free:  {mem_usages['usage_free']}{inline_swap_bar[3]}      free:  {swap_usages['usage_free']}
-    
+
+NETWORK                         State        TASKS {get_task_number()} ({get_threads()} threads), {get_running_tasks()} running, {get_sleeping_tasks()} sleeping
+Connected:{inline_connection_bar}{is_connected(True)}
+Ping time:{inline_ping_bar}{get_ping_time()}
+
+FILE SYS                 Used   Total
+{get_file_system_info()}
 {time()} {get_timezone()}
     '''
 
--- system/_inet.py
+++ system/_inet.py
@@ -1,6 +1,47 @@
-import socket
+import speedtest, socket
+
+from colorama import Fore, Style
 
 
 def get_ipv4():
-    hostname = socket.gethostname()
-    return socket.gethostbyname(hostname)
+    if is_connected():
+        hostname = socket.gethostname()
+        return socket.gethostbyname(hostname)
+    else:
+        return '...'
+
+
+def is_connected(formated=False):
+    try:
+        socket.setdefaulttimeout(3)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect(("8.8.8.8", 53))
+
+        if formated:
+            return f"{Fore.GREEN}True{Style.RESET_ALL}"
+        else:
+            return True
+    except socket.error:
+        if formated:
+            return f"{Fore.RED}True{Style.RESET_ALL}"
+        else:
+            return False
+
+
+def get_ping_time(raw=False):
+    if is_connected():
+        st = speedtest.Speedtest()
+        st.get_servers([])
+
+        ping = str(st.results.ping) + 'ms'
+
+        if raw:
+            return ping
+        else:
+            if st.results.ping <= 60:
+                return f"{Fore.GREEN}{str(st.results.ping) + 'ms'}{Style.RESET_ALL}"
+            elif 60 < st.results.ping <= 150:
+                return f"{Fore.YELLOW}{str(st.results.ping) + 'ms'}{Style.RESET_ALL}"
+            else:
+                return f"{Fore.RED}{str(st.results.ping) + 'ms'}{Style.RESET_ALL}"
+    else:
+        return '...'
